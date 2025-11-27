652438a2fa316545ebcea424e0826539a0f39c21
JiyeonJeong01 <jizzvibe@gmail.com>
2025-11-27 18:17:54 +0900

    fix : ImGui 창 꺼짐 문제 해결
    Begin() 과 End() 짝이 맞지 않은 문제 해결

--- Engine/Code/CMediator.cpp
+++ Engine/Code/CMediator.cpp
@@ -26,35 +26,33 @@ void CMediator::Display_AllScenePanel()
         ImGui::BeginChild("LeftColumn", ImVec2(200, 0), true); 
         {
             ImGui::BeginChild("Scene Type", ImVec2(0, 50), true);
-
-
-            ImGui::Text("Scene : "); ImGui::SameLine();
-            ImGui::PushItemWidth(80);
-            if (ImGui::BeginCombo("##", m_vecScene[m_dwCurSceneIdx].c_str()))
             {
-                for (_uint dwIdx = 0; dwIdx < m_dwEndScene; dwIdx++)
+                ImGui::Text("Scene : "); ImGui::SameLine();
+                ImGui::PushItemWidth(80);
+                if (ImGui::BeginCombo("##", m_vecScene[m_dwCurSceneIdx].c_str()))
                 {
-                    _bool bSelected = m_dwCurSceneIdx == dwIdx;
-
-                    if (ImGui::Selectable(m_vecScene[dwIdx].c_str(), bSelected))
+                    for (_uint dwIdx = 0; dwIdx < m_dwEndScene; dwIdx++)
                     {
-                        m_dwCurSceneIdx = dwIdx;
-                        Change_Scene(m_dwCurSceneIdx);
-                    }
+                        _bool bSelected = m_dwCurSceneIdx == dwIdx;
 
-                    if (bSelected)
-                        ImGui::SetItemDefaultFocus();
+                        if (ImGui::Selectable(m_vecScene[dwIdx].c_str(), bSelected))
+                        {
+                            m_dwCurSceneIdx = dwIdx;
+                            Change_Scene(m_dwCurSceneIdx);
+                        }
+
+                        if (bSelected)
+                            ImGui::SetItemDefaultFocus();
+                    }
+                    ImGui::EndCombo();
+                    ImGui::PopItemWidth();
                 }
-                ImGui::EndCombo();
-                ImGui::PopItemWidth();
             }
             ImGui::EndChild();
-
-            ImGui::EndChild();
         }
-        ImGui::End();
-
+        ImGui::EndChild();
     }
+    ImGui::End();
 }
 
 void CMediator::Change_Scene(_uint dwSceneIdx)

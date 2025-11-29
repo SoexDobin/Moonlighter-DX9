1d54c21b62e483ac2e144e57b48cb8977441d918
JiyeonJeong01 <jizzvibe@gmail.com>
2025-11-30 02:08:00 +0900

    feat : 보스 미니언 기본 틀

---
+++ Client/Code/CBossMinion.cpp
@@ -0,0 +1,153 @@
+﻿#include "pch.h"
+#include "CBossMinion.h"
+
+#include "CHitRectBox.h"
+
+CBossMinion::CBossMinion(LPDIRECT3DDEVICE9 pGraphicDev, CRenderObject* pOwner)
+    : CMonster(pGraphicDev)
+    , m_pDynamicTexCom(nullptr),m_pOwner(pOwner)
+{
+}
+
+CBossMinion::CBossMinion(const CBossMinion& rhs)
+    : CMonster(rhs)
+    , m_pDynamicTexCom(nullptr), m_pOwner(rhs.m_pOwner)
+{
+}
+
+CBossMinion::~CBossMinion()
+{
+}
+
+HRESULT CBossMinion::Ready_GameObject()
+{
+    if (FAILED(CRenderObject::Ready_GameObject()))
+        return E_FAIL;
+
+    m_pTransformCom->Set_Scale({ 2.f, 2.f, 2.f });
+
+    Ready_Combat();
+    Ready_Texture();
+
+    m_iObjectID = OBJECT_ID::MONSTER;
+
+    m_bAscending = m_bFalling = m_bAnchored = false;
+
+    return S_OK;
+}
+
+_int CBossMinion::Update_GameObject(const _float fTimeDelta)
+{
+    _int iExit = CRenderObject::Update_GameObject(fTimeDelta);
+
+    if (m_bAscending)
+    {
+        Ascending(fTimeDelta);
+        return 0;
+    }
+
+    if (m_bFalling)
+    {
+        Falling(fTimeDelta);
+        return 0;
+    }
+
+    if (m_bAnchored)
+    {
+        Anchored(fTimeDelta);
+        return 0;
+    }
+
+    return iExit;
+}
+
+void CBossMinion::LateUpdate_GameObject(const _float fTimeDelta)
+{
+    CRenderObject::LateUpdate_GameObject(fTimeDelta);
+}
+
+void CBossMinion::Render_GameObject()
+{
+    m_pGraphicDevice->SetTransform(D3DTS_WORLD, m_pTransformCom->Get_World());
+
+    m_pGraphicDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
+    m_pGraphicDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+    m_pGraphicDevice->SetRenderState(D3DRS_ALPHATESTENABLE, TRUE);
+
+    m_pDynamicTexCom->SetUp_Texture();
+    m_pBufferCom->Render_Buffer();
+
+    m_pGraphicDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
+}
+
+void CBossMinion::On_Collision(const Collision& tCollision)
+{
+    // 플레이어 공격 충돌 시 hp 깎기 
+}
+
+void CBossMinion::Ready_Combat()
+{
+
+}
+
+void CBossMinion::Ready_Texture()
+{
+    m_pDynamicTexCom = Add_Component<CTexture>(ID_DYNAMIC, L"Texture_Com", TEXTURE);
+
+    vector<wstring> vecTexture = { L"Boss_Cutting_Growing", L"Boss_Cutting_Idle", L"Boss_Cutting_Shot"};
+
+    for (auto& texture : vecTexture)
+        m_pDynamicTexCom->Ready_Texture(texture);
+}
+
+void CBossMinion::Ascending(const _float fTimeDelta)
+{
+    m_pTransformCom->Move_Pos(&vDir, fTimeDelta, m_fSpeed);
+
+    if (m_pTransformCom->Get_Pos().y >= m_fMaxHeight)
+    {
+        _vec3 vNewPos = m_vAnchorPos;
+        vNewPos.y = m_pTransformCom->Get_Pos().y;
+        m_pTransformCom->Set_Pos(vNewPos);
+
+        vDir *= -1; // 방향 전환
+        m_bFalling = true;
+        m_bAscending = false;
+    }
+}
+
+void CBossMinion::Falling(const _float fTimeDelta)
+{
+    m_pTransformCom->Move_Pos(&vDir, fTimeDelta, m_fSpeed);
+
+    if (m_pTransformCom->Get_Pos().y <= m_vAnchorPos.y + m_fEpsilon)
+    {
+
+        vDir *= 0.f;
+        m_bFalling = true;
+        m_bAscending = false;
+    }
+}
+
+void CBossMinion::Anchored(const _float fTimeDelta)
+{
+}
+
+CBossMinion* CBossMinion::Create(LPDIRECT3DDEVICE9 pGraphicDev, CRenderObject* pOwner)
+{
+    CBossMinion* pObj = new CBossMinion(pGraphicDev, pOwner);
+
+    if (FAILED(pObj->Ready_GameObject()))
+    {
+        Safe_Release(pObj);
+        MSG_BOX("CBossMinion Create Failed!!");
+        return nullptr;
+    }
+
+    return pObj;
+}
+
+void CBossMinion::Free()
+{
+    CRenderObject::Free();
+}
---
+++ Client/Header/CBossMinion.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#include "CMonster.h"
+
+class CHitRectBox;
+
+class CBossMinion : public CMonster
+{
+private:
+    explicit CBossMinion(LPDIRECT3DDEVICE9 pGraphicDev, CRenderObject* pOwner);
+    explicit CBossMinion(const CBossMinion& rhs);
+    virtual ~CBossMinion();
+
+public:
+    HRESULT     Ready_GameObject() override;
+    _int        Update_GameObject(const _float fTimeDelta) override;
+    void        LateUpdate_GameObject(const _float fTimeDelta) override;
+    void        Render_GameObject() override;
+
+    void        On_Collision(const Collision& tCollision) override;
+
+private :
+    void Ready_EntityComponent() override {};
+    void Configure_Component() override {};
+public:
+    void        Shoot() { m_bAscending = true; }
+    void        Set_AnchorPos(const _vec3& vPos) { m_vAnchorPos = vPos; }
+
+private:
+    void        Ready_Combat();
+    void        Ready_Texture();
+
+    void        Ascending(const _float fTimeDelta);
+    void        Falling(const _float fTimeDelta);
+    void        Anchored(const _float fTimeDelta);
+
+private:
+    CTexture* m_pDynamicTexCom;     // 텍스처
+
+private:
+    CRenderObject* m_pOwner;
+    DAMAGE_INFO m_tDamage;              // 미니언이 발사할 투사체의 데미지 정보
+
+    _bool       m_bAscending, m_bFalling, m_bAnchored;
+    _float      m_bLastShootTime, m_bLimitShootTime;
+
+    _float      m_fSpeed;
+    _float      m_fMaxHeight;
+    _vec3 vDir = { 0.f, 1.f, 0.f },   m_vAnchorPos;
+    const _float      m_fEpsilon = 0.1f;
+
+
+
+public:
+    static CBossMinion* Create(LPDIRECT3DDEVICE9 pGraphicDev, CRenderObject* pOwner);
+
+protected:
+    void                Free() override;
+};
+
